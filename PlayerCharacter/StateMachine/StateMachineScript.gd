4017e350feb914048c3fce44bbc8cec63d0de799
ttzytt <Zyt2006613@outlook.com>
2025-05-28 15:16:05 -0400

    [WIP]: debugging rpc

--- PlayerCharacter/StateMachine/StateMachineScript.gd
+++ PlayerCharacter/StateMachine/StateMachineScript.gd
@@ -7,32 +7,62 @@ var currStateName  : String
 var states : Dictionary = {}
 
 @onready var charRef : CharacterBody3D = $".."
-var ls = LogStream.new("StateMachine")
+var ls = LogStream.new("StateMachine", LogStream.LogLevel.DEBUG)
+
+# func _ready():
+# 	rpc_ready.rpc()
 
 func _ready():
 	#get all the state childrens
 	for child in get_children():
 		if child is State:
 			states[child.name.to_lower()] = child
-			child.transitioned.connect(onStateChildTransition)
+			child.transitioned.connect(onStateChildTransition.rpc)
 			
 	#if initial state, transition to it
 	if initialState:
 		initialState.enter(charRef)
 		currState = initialState
 		currStateName = currState.stateName
-		
+
+func reconnectTransition():
+	#connect all the transitions of the states
+	ls.dbg("reconnecting state transitions by peer %d" % get_tree().get_multiplayer().get_unique_id())
+	ls.dbg("all connected peers: %s" % get_tree().get_multiplayer().get_peers())
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.transitioned.disconnect(onStateChildTransition.rpc)
+			child.transitioned.connect(onStateChildTransition.rpc)
+
+@rpc("any_peer", "call_remote")
+func test_rpc():
+	#this is just a test rpc to see if the rpc works
+	ls.dbg("test_rpc called by peer %d" % get_tree().get_multiplayer().get_unique_id())
+	ls.dbg("all connected peers: %s" % get_tree().get_multiplayer().get_peers())
+
 func _process(delta : float):
+	rpc_process.rpc(delta)
+
+@rpc("any_peer", "call_local")		
+func rpc_process(delta: float):
 	if currState: currState.update(delta)
 	
 func _physics_process(delta: float):
-	if currState: currState.physics_update(delta)
+	rpc_physics_process.rpc(delta)
 	
+@rpc("any_peer", "call_local")
+func rpc_physics_process(delta: float):
+	if currState: currState.physics_update(delta)
+
+@rpc("any_peer", "call_local", "reliable")
 func onStateChildTransition(state : State, newStateName : String):
 	#manage the transition from one state to another
 	
 	if state != currState: return
-	
+	ls.dbg("state child transtion of peer %d got called" % get_tree().get_multiplayer().get_unique_id())
+	ls.dbg("all connected peers: %s" % get_tree().get_multiplayer().get_peers())
+
 	var newState = states.get(newStateName.to_lower())
 	if !newState: return
 	
--- scripts/game/main.gd
+++ scripts/game/main.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
 	multiplayer.multiplayer_peer = multiplayer_peer
+	multiplayer.allow_object_decoding = true
 	ls.info("multiplayer_peer set")
 	add_child(map_manager)
 	add_player(multiplayer.get_unique_id()) # add the local player
@@ -29,7 +30,12 @@ func add_player(player_id : int) -> void:
 	player.position = spawn_pt
 	ls.info("Adding player with ID: %d at " % player_id, spawn_pt)
 	add_child(player)
-	ls.info("This instance's authority on the new player[id=%d] is" % player_id, player.is_multiplayer_authority())	
+	ls.info("This instance's authority on the new player[id=%d] is " % player_id, player.is_multiplayer_authority())	
+
+	# reconnect transition signal 
+	var authority_player : PlayerCharacter = player_dict.get(multiplayer.get_unique_id())
+	authority_player.get_node("StateMachine").reconnectTransition()
+	authority_player.get_node("StateMachine").test_rpc.rpc()
 
 func remove_player(player_id : int) -> void:
 	ls.info("Removing player with ID: %d" % player_id)
